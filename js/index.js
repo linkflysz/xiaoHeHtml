34f35b6e25c948be6bc4d460f276c9399eedba15
linkfly <2588132@qq.com>
2018-08-06 17:48:40 +0800

    init in 20180806

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+	
+	var accordion = new Accordion($('#accordion2'), false);//同一页面加两个 list cgliu
+	
+} );
